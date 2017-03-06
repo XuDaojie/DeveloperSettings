c1d041ef2e3e22833bc099b93e2334eeb438a7c7
XuDaojie <xudaojieg@gmail.com>
2017-03-06 17:19:57 +0800

    实现指针位置、显示点按操作的视觉反馈

--- app/src/main/java/io/github/xudaojie/developersettings/TestPreferenceActivity.java
+++ app/src/main/java/io/github/xudaojie/developersettings/TestPreferenceActivity.java
@@ -1,6 +1,8 @@
 package io.github.xudaojie.developersettings;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 
 /**
@@ -8,12 +10,30 @@ import android.support.annotation.Nullable;
  */
 
 public class TestPreferenceActivity extends AppCompatPreferenceActivity {
+    private static final String TAG = "TestPreferenceActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 //        addPreferencesFromResource(R.xml.pref_test);
         addPreferencesFromResource(R.xml.pref_test);
+        findPreference("show_touches").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Settings.System.putInt(getContentResolver(), "pointer_location",
+                        newValue == Boolean.TRUE ? 1 : 0);
+                return true;
+            }
+        });
+        findPreference("point_location").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Settings.System.putInt(getContentResolver(), "show_touches",
+                        newValue == Boolean.TRUE ? 1 : 0);
+                return true;
+            }
+        });
     }
 
 }
